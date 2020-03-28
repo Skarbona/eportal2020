61498431b91261c6d94b4b2dc308d21c36866ed8
Filip Sokołowski <34159584+Skarbona@users.noreply.github.com>
2020-03-28 13:45:16 +0100

    React Router + Handling Pages (#15)
    * Add react-router
    
    * Add react-router
    
    * v0.2.2

---
+++ packages/client/src/components/Pages/AuthPage/AuthPage.tsx
@@ -0,0 +1,7 @@
+import React, { FC, memo } from 'react';
+
+export const AuthPageComponent: FC<{}> = () => {
+  return <div>AUTH PAGE</div>;
+};
+
+export default memo(AuthPageComponent);
---
+++ packages/client/src/components/Pages/Main/Main.tsx
@@ -0,0 +1,7 @@
+import React, { FC, memo } from 'react';
+
+export const MainComponent: FC<{}> = () => {
+  return <div>HOME PAGE</div>;
+};
+
+export default memo(MainComponent);
