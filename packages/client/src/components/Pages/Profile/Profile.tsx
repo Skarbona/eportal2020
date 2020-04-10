9ef93fd772658cdce9bfcaa08ebb5b441c6b04d1
Filip <filip.sokolowski1990@gmail.com>
2020-04-10 15:20:25 +0200

    fix

--- packages/client/src/components/Pages/Profile/Profile.tsx
+++ packages/client/src/components/Pages/Profile/Profile.tsx
@@ -1,5 +1,4 @@
 import React, { FC, memo, Fragment } from 'react';
-import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { Grid } from '@material-ui/core';
 
