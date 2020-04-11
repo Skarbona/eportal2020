2f61c1f68743301d1de22447532bdfb836ff8ff7
Filip <filip.sokolowski1990@gmail.com>
2020-04-11 20:53:25 +0200

    Add switcher for profile page, change approach to mobile first (CSS)

---
+++ packages/client/src/utils/profile-settings.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import { Box } from '@material-ui/core';
+
+import { ProfileSettings } from '../components/Pages/Profile/Settings';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: ProfileSettings;
+  value: ProfileSettings;
+}
+
+export const a11yProps = (index: ProfileSettings) => ({
+  id: `scrollable-auto-tab-${index}`,
+  'aria-controls': `scrollable-auto-tabpanel-${index}`,
+});
+
+export const TabPanel: FC<TabPanelProps> = ({ children, value, index }) => (
+  <div
+    role="tabpanel"
+    hidden={value !== index}
+    id={`scrollable-auto-tabpanel-${index}`}
+    aria-labelledby={`scrollable-auto-tab-${index}`}
+  >
+    {value === index && <Box p={3}>{children}</Box>}
+  </div>
+);
