2a3255a5c022f1b06fb39a118dadb9c8781d1209
Filip <filip.sokolowski1990@gmail.com>
2020-04-15 16:21:54 +0200

    edit codecov file

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 [![lerna](https://img.shields.io/badge/maintained%20with-lerna-cc00ff.svg)](https://lerna.js.org/)
 
+##### Client Test coverage:
 
-##### Client Test coverage: 
 [![codecov](https://codecov.io/gh/Skarbona/eportal2020/branch/master/graph/badge.svg?flag=client)](https://codecov.io/gh/Skarbona/eportal2020)
 
+##### Service Test coverage:
+
+[![codecov](https://codecov.io/gh/Skarbona/eportal2020/branch/master/graph/badge.svg?flag=service)](https://codecov.io/gh/Skarbona/eportal2020)
 
 ## How to start ?
+
 1. Run `npm i`
 2. Run `lerna bootstrap`. If command not working install lerna globally `npm i -g lerna` or run scripts from main package.json.
 
 ## How to develop?
+
 1. Run `lerna run start`
 2. Or access specific package folder and run `npm run start`
 
 ## How to test?
+
 1. Run `lerna run test`
 2. Or access specific package folder and run `npm run test`
--- packages/service/src/tests/integretion/controllers/users.test.ts
+++ packages/service/src/tests/integretion/controllers/users.test.ts
@@ -7,6 +7,16 @@ import User from '../../../models/user';
 
 const endpoint = '/api/users/';
 
+const signUpUser = (server: any) => {
+  return request(server)
+    .post(endpoint + '/signup')
+    .send({
+      password: 'aaAA1111',
+      userName: 'AAAA',
+      email: 'test@test.pl',
+    });
+};
+
 describe('Controller: Users', () => {
   let server: any;
 
@@ -25,13 +35,7 @@ describe('Controller: Users', () => {
 
   describe('signUp Controller', () => {
     it('should signUp user', async () => {
-      const response = await request(server)
-        .post(endpoint + '/signup')
-        .send({
-          password: 'aaAA1111',
-          userName: 'AAAA',
-          email: 'test@test.pl',
-        });
+      const response = await signUpUser(server);
 
       expect(response.status).toEqual(201);
       expect(response.body.userData.gameDefaults).toBeDefined();
@@ -78,13 +82,8 @@ describe('Controller: Users', () => {
     });
 
     it('should NOT signUp user if userName or Email already exist', async () => {
-      await request(server)
-        .post(endpoint + '/signup')
-        .send({
-          password: 'aaAA1111',
-          userName: 'AAAA',
-          email: 'test@test.pl',
-        });
+      await signUpUser(server);
+
       const userNameExist = await request(server)
         .post(endpoint + '/signup')
         .send({
@@ -107,13 +106,7 @@ describe('Controller: Users', () => {
 
   describe('Login Controller', () => {
     it('should Login user', async () => {
-      await request(server)
-        .post(endpoint + '/signup')
-        .send({
-          password: 'aaAA1111',
-          userName: 'AAAA',
-          email: 'test@test.pl',
-        });
+      await signUpUser(server);
 
       const response = await request(server)
         .post(endpoint + '/login')
@@ -133,19 +126,14 @@ describe('Controller: Users', () => {
       expect(response.body.accessToken).toBeDefined();
       expect(response.body.refreshToken).toBeDefined();
     });
+
     it('should NOT Login user if not correct data provided', async () => {
-      await request(server)
-        .post(endpoint + '/signup')
-        .send({
-          password: 'aaAA1111',
-          userName: 'AAAA',
-          email: 'test@test.pl',
-        });
+      await signUpUser(server);
 
       const invalidPassword = await request(server)
         .post(endpoint + '/login')
         .send({
-          password: 'aaAA1111',
+          password: 'aaAA1111111',
           email: 'test@test.pl',
         });
 
@@ -158,6 +146,119 @@ describe('Controller: Users', () => {
 
       expect(invalidPassword.status).toEqual(401);
       expect(notExistEmail.status).toEqual(401);
+    });
+
+    it('should NOT Login user if body data are not correct', async () => {
+      await signUpUser(server);
+
+      const emptyBody = await request(server)
+        .post(endpoint + '/login')
+        .send();
+      const invalidEmail = await request(server)
+        .post(endpoint + '/login')
+        .send({
+          password: 'aaAA1111',
+          email: 'test@test',
+        });
+      const invalidPassword = await request(server)
+        .post(endpoint + '/login')
+        .send({
+          password: 'aaAA11',
+          email: 'test@test.pl',
+        });
+      expect(emptyBody.status).toEqual(400);
+      expect(invalidEmail.status).toEqual(400);
+      expect(invalidPassword.status).toEqual(400);
+    });
+  });
+
+  describe('getUserData Controller', () => {
+    it('should return userData', async () => {
+      const user = await signUpUser(server);
+
+      const response = await request(server)
+        .get(endpoint)
+        .set('Authorization', `Bearer ${user.body.accessToken}`);
+
+      expect(response.status).toEqual(200);
+      expect(response.body.user).toBeDefined();
+      expect(response.body.user.email).toEqual('test@test.pl');
+      expect(response.body.user.name).toEqual('AAAA');
+      expect(response.body.user.type).toEqual('user');
+      expect(response.body.user.id).toBeDefined();
+      expect(response.body.user.gameDefaults).toBeDefined();
+      expect(response.body.user.date).toBeDefined();
+      expect(response.body.user.password).not.toBeDefined();
+    });
+
+    it('should NOT return userData if access token not provided', async () => {
+      await signUpUser(server);
+
+      const response = await request(server).get(endpoint);
+
+      expect(response.status).toEqual(401);
+    });
+  });
+
+  describe('updateUser Controller', () => {
+    it('should update user password', async () => {
+      const user = await signUpUser(server);
+
+      const response = await request(server)
+        .patch(endpoint)
+        .send({
+          password: 'NewPassword11',
+        })
+        .set('Authorization', `Bearer ${user.body.accessToken}`);
+
+      const loginUser = await request(server)
+        .post(endpoint + '/login')
+        .send({
+          password: 'NewPassword11',
+          email: 'test@test.pl',
+        });
+
+      expect(response.status).toEqual(200);
+      expect(loginUser.status).toEqual(200);
+      expect(response.body.user).toBeDefined();
+    });
+
+    it('should NOT update userData if access token not provided', async () => {
+      await signUpUser(server);
+
+      const response = await request(server).patch(endpoint).send({
+        password: 'NewPassword11',
+      });
+
+      expect(response.status).toEqual(401);
+    });
+  });
+
+  describe('deleteUser Controller', () => {
+    it('should delete user', async () => {
+      const user = await signUpUser(server);
+
+      const response = await request(server)
+        .delete(endpoint)
+        .set('Authorization', `Bearer ${user.body.accessToken}`);
+
+      const loginUser = await request(server)
+        .post(endpoint + '/login')
+        .send({
+          password: 'aaAA1111',
+          email: 'test@test.pl',
+        });
+
+      expect(response.status).toEqual(200);
+      expect(loginUser.status).toEqual(401);
+    });
+
+    it('should NOT delete user if access token not provided', async () => {
+      await signUpUser(server);
+
+      const response = await request(server).delete(endpoint);
+
+      expect(response.status).toEqual(401);
     });
   });
 });
