5fd4dff3ff7cfe719ee8fbbad8c6788e45bb578b
Filip <filip.sokolowski1990@gmail.com>
2020-04-24 08:34:19 +0200

    remove not used variable

--- packages/client/src/store/game/thunks/startGame.ts
+++ packages/client/src/store/game/thunks/startGame.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { setUserData } from '../../user/thunks/setUserData';
 import { AppThunk } from '../../store.interface';
-import { LocalStorage } from '../../../models/local-storage';
 import { fetchPostsForGame } from './fetchPostsForGame';
 
 export const startGameHandler = (): AppThunk => async (dispatch, getState) => {
