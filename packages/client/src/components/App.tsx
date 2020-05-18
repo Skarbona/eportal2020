240ba427bc19db8a4443783ccb0373ae01d8a436
Filip <filip.sokolowski1990@gmail.com>
2020-05-18 13:44:25 +0200

    changes for production

--- packages/client/src/components/App.tsx
+++ packages/client/src/components/App.tsx
@@ -2,7 +2,6 @@ import React, { FC, useEffect } from 'react';
 import { Container, useMediaQuery } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import { useSelector } from 'react-redux';
-import { useLocation, BrowserRouter as Router } from 'react-router-dom';
 
 import './App.scss';
 
@@ -25,26 +24,7 @@ interface AppSelector {
   expirationTokenDate: Date;
 }
 
-class DebugRouter extends Router {
-  constructor(props: any) {
-    super(props);
-    // @ts-ignore-next-line;
-    console.log('initial history is: ', JSON.stringify(this.history, null, 2));
-    // @ts-ignore-next-line;
-    this.history.listen((location, action) => {
-      console.log(`The current URL is ${location.pathname}${location.search}${location.hash}`);
-      console.log(
-        `The last navigation action was ${action}`,
-        // @ts-ignore-next-line;
-        JSON.stringify(this.history, null, 2),
-      );
-    });
-  }
-}
-
 export const App: FC = () => {
-  let location = useLocation();
-  console.log('LOCATION', location);
   const { id, accessToken, expirationTokenDate } = useSelector<RootState, AppSelector>(
     ({ user, app }) => ({
       id: user.userData.id,
@@ -62,21 +42,19 @@ export const App: FC = () => {
   }, [accessToken, expirationTokenDate, id]);
 
   return (
-    <DebugRouter>
-      <ThemeProvider theme={theme}>
-        <>
-          <AuthHOC />
-          <ScrollToTop />
-          <Header accessToken={accessToken} />
-          <Container component="main" className="eportal__main" maxWidth={false} disableGutters>
-            <Pages accessToken={accessToken} expirationDate={expirationTokenDate} />
-            <SnackBarErrorHandler />
-          </Container>
-          <Footer />
-          {isMobile && <BottomNavigation accessToken={accessToken} />}
-        </>
-      </ThemeProvider>
-    </DebugRouter>
+    <ThemeProvider theme={theme}>
+      <>
+        <AuthHOC />
+        <ScrollToTop />
+        <Header accessToken={accessToken} />
+        <Container component="main" className="eportal__main" maxWidth={false} disableGutters>
+          <Pages accessToken={accessToken} expirationDate={expirationTokenDate} />
+          <SnackBarErrorHandler />
+        </Container>
+        <Footer />
+        {isMobile && <BottomNavigation accessToken={accessToken} />}
+      </>
+    </ThemeProvider>
   );
 };
 
