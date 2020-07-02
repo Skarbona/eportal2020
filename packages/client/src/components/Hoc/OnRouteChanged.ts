7203b66c33633841f21f5760cc4a8e9caddacf2b
Filip <filip.sokolowski1990@gmail.com>
2020-07-02 14:13:20 +0200

    Add Google Analytics

--- packages/client/src/components/Hoc/OnRouteChanged.ts
+++ packages/client/src/components/Hoc/OnRouteChanged.ts
@@ -1,14 +1,20 @@
 import { useEffect, FC, memo } from 'react';
 import { useHistory } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import { cleanAlertsHandler } from '../../store/app/thunks/cleanAlerts';
 import { useReduxDispatch } from '../../store/helpers';
+import { GOOGLE_ANALYTICS } from '../../constants/envs';
+
+ReactGA.initialize(GOOGLE_ANALYTICS);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 export const OnRouteChangedComponent: FC = () => {
   const history = useHistory();
   const dispatch = useReduxDispatch();
   useEffect(() => {
     const unListen = history.listen(() => {
+      ReactGA.pageview(window.location.pathname + window.location.search);
       window.scrollTo(0, 0);
       dispatch(cleanAlertsHandler());
     });
