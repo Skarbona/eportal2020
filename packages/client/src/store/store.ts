87ea7102209579747c0621ac34f35c4f4db1451b
Filip <filip.sokolowski1990@gmail.com>
2021-12-19 09:58:17 +0100

    Fix tests

--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -19,7 +19,7 @@ export const initialRootState: RootState = {
   waitingRoom: waitingRoomInitialState,
 };
 
-export const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore<RootState, Actions, null, AnyAction>(
   reducers,
