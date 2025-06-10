1fa6f978125bb8fb1a6c951042e2f42f2dd2a159
Filip Sokołowski <filip.sokolowski1990@gmail.com>
2025-06-10 13:57:15 +0300

    add more logs

--- packages/service/src/utils/getOrCreateCustomer.ts
+++ packages/service/src/utils/getOrCreateCustomer.ts
@@ -6,13 +6,16 @@ export const getOrCreateCustomer = async (
   userId: string,
   params?: Stripe.CustomerCreateParams,
 ): Promise<Stripe.Response<Stripe.Customer>> => {
+  console.error('getOrCreateCustomer called for userId:', userId);
   const user = await User.findById(userId);
+  console.error('getOrCreateCustomer found user:', user);
 
   const { stripeCustomerId, email } = user;
 
   // If missing customerID, create it
   if (!stripeCustomerId) {
     // CREATE new customer
+    console.warn('No stripeCustomerId, creating new Stripe customer for email:', email);
     const customer = await stripe.customers.create({
       email,
       metadata: {
@@ -22,8 +25,10 @@ export const getOrCreateCustomer = async (
     });
     user.stripeCustomerId = customer.id;
     await user.save();
+    console.warn('Created new Stripe customer:', customer.id);
     return customer;
   } else {
+    console.warn('stripeCustomerId exists, retrieving Stripe customer:', stripeCustomerId);
     return (await stripe.customers.retrieve(stripeCustomerId)) as Stripe.Response<Stripe.Customer>;
   }
 };
